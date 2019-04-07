76177372f4a341722d55c7b8b4a7dbc56ddded26
hiya5150 <5828537@gmail.com>
2019-04-06 23:59:47 -0400

    all done

--- process.php
+++ process.php
@@ -1,5 +1,29 @@
-<?php
+    <?php
 
 echo 'Processing...';
+
+//connnect to db
+
+$conn = mysqli_connect('localhost', 'root', '123456', 'ajaxtest');
+
+//Check for get variable
+
+if(isset($_GET['name'])) {
+    echo 'GET: Your name is ' . $_GET['name'];
+}
+if(isset($_POST['name'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+//    echo 'POST: Your name is ' . $_POST['name'];
+    
+    $query = "INSERT into users(name) VALUES('$name')";
+    
+    if(mysqli_query($conn, $query)) {
+        echo 'User added';
+    }
+    else {
+        echo 'ERROR: '.mysqli_error($conn);
+    }
+    
+}
 
 ?>
---
+++ users.php
@@ -0,0 +1,13 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', '123456', 'ajaxtest');
+
+// query db
+$query = 'SELECT * FROM users';
+
+// get result
+$result = mysqli_query($conn, $query);
+
+//fetch data
+$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo json_encode($users);
